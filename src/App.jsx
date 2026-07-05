2a31b90cdd9f5a4b4d764e2fb401030fe97f2af9
Akshat Kumar Sachdeva <akshatkumarsachdeva@gmail.com>
2026-07-05 16:25:51 +0530

    implement welcome section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import Navbar from '#components/Navbar'
+import { Navbar, Welcome } from '#components'
 
 const App = () => {
   return (
     <main>
       <Navbar/>
+      <Welcome/>
     </main>
-  )
+  ) 
 }
-
 export default App
+
   
